2ff2c34fb6e7590a1094d27a81c78d916beb6070
afuap <131736706+afuap@users.noreply.github.com>
2024-05-09 17:42:43 -0400

    Updated Meaning.js

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,7 @@ export default function Results(props) {
       <div className="Results">
         <h2>{props.results.word}</h2>
         {props.results.phonetic}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
