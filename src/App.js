63ba68c2d5766f42a7be524de39f146d00b6d997
afuap <131736706+afuap@users.noreply.github.com>
2024-05-09 22:04:10 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,27 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="beautiful" />
         </main>
+        <footer className="App-footer">
+          <small>
+            Coded by Georgina Pobi is{" "}
+            <a
+              href="https://github.com/afuap/dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              opened sourced on GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://warm-concha-2ded23.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+            .
+          </small>
+        </footer>
       </div>
     </div>
   );
